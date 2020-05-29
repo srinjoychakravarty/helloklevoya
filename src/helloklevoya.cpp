8cf44f7e7c62643e24d3a261f0bfc7af798cd9f3
Srinjoy Chakravarty <chakravarty.s@northeastern.edu>
2020-05-29 15:47:44 -0400

    :construction: age assertion added

--- src/helloklevoya.cpp
+++ src/helloklevoya.cpp
@@ -11,6 +11,13 @@ void helloklevoya::addpet(uint64_t const _id, eosio::name const & _owner, eosio:
   // 1st Param: table in current smart contract 
   // 2nd Param: scope of table is global i.e. anyone can access
   pets_table pets(get_self(), get_self().value);
+ 
+  // searches table for id (primary key)
+  auto pet_iterator = pets.find(_id);
+
+  // ensures same pet id doesn't exist in table before allowing insertion
+  eosio::check(pet_iterator == pets.end(), "Pet ID already exists...can't register again!");
+ 
   // 1st Param: payer of RAM
   // 2nd Param: lambda function which sets table entry type to pet_t
   pets.emplace(get_self(), [&](auto & entry) 
@@ -22,8 +29,6 @@ void helloklevoya::addpet(uint64_t const _id, eosio::name const & _owner, eosio:
 void helloklevoya::updatepet(uint64_t const _id, eosio::name const & _owner, eosio::name const &  _pet_name, uint64_t const _age, eosio::name const & _type)
 {
   pets_table pets(get_self(), get_self().value);
-
-  // searches table for id (primary key)
   auto pet_iterator = pets.find(_id);
 
   // ensures pet exists in table before allowing update
@@ -31,6 +36,9 @@ void helloklevoya::updatepet(uint64_t const _id, eosio::name const & _owner, eos
 
   // allows the actual owner of the pet to update
   require_auth(pet_iterator -> get_owner());
+
+  // prevents pets getting younger with passage of time
+  eosio::check(_age >= pet_iterator -> get_age(), "Pet cannot get younger over time");
 
   // if found iterator id from pets table is passed in as additional first argument
   pets.modify(pet_iterator, get_self(), [&](auto & entry) 
